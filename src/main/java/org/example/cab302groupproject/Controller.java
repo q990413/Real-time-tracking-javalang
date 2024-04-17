08b1d8d92835b9364f29cbb25c016ae3208eeda5
CyberStorm21 <alex.spence@outlook.com>
2024-04-17 15:56:06 +1000

    Added simple popup window with editable text

--- src/main/java/org/example/cab302groupproject/Controller.java
+++ src/main/java/org/example/cab302groupproject/Controller.java
@@ -1,14 +1,30 @@
 package org.example.cab302groupproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button NotificationTestButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void testingNotificationButton() throws IOException {
+        Stage stage = (Stage) NotificationTestButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(Model.class.getResource("Notification.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
     }
 }
---
+++ src/main/java/org/example/cab302groupproject/NotificationController.java
@@ -0,0 +1,45 @@
+package org.example.cab302groupproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import java.io.IOException;
+
+public class NotificationController {
+    @FXML
+    private TextArea notificationWords;
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    public void initialize() {
+        notificationWords.setText("WRITE HERE FOR POPUP WINDOW");
+    }
+
+    @FXML
+    protected void onPopupButtonClick() {
+        Stage popupStage = new Stage();
+        VBox popupRoot = new VBox();
+        Label label = new Label(notificationWords.getText());
+        popupRoot.getChildren().add(label);
+        Scene scene = new Scene(popupRoot, 200, 100);
+
+        popupStage.setTitle("Popup");
+        popupStage.setScene(scene);
+
+        // Set modal behavior
+        popupStage.initModality(Modality.WINDOW_MODAL);
+        popupStage.initOwner(stage);
+
+        popupStage.showAndWait();
+    }
+}
